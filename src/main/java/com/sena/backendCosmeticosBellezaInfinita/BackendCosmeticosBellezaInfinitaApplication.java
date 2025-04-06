14bdf6b4fc476160d4771c8069d2b88703876227
breiner-r <breinerr006@gmail.com>
2025-04-06 18:47:58 -0500

    first commit

---
+++ src/main/java/com/sena/backendCosmeticosBellezaInfinita/BackendCosmeticosBellezaInfinitaApplication.java
@@ -0,0 +1,13 @@
+package com.sena.backendCosmeticosBellezaInfinita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCosmeticosBellezaInfinitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCosmeticosBellezaInfinitaApplication.class, args);
+	}
+
+}
